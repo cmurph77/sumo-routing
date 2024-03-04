3906b10dc0942ded88fac908b049d305b2e6d2d4
cmurph77 <91293683+cmurph77@users.noreply.github.com>
2024-03-04 16:43:11 +0000

    removed unnessasary files

--- exp2/run_rand_20_crva.sh
+++
@@ -1,9 +0,0 @@
-python3 cr_v2.py 500 rand_20 5 Y
-python3 cr_v2.py 1000 rand_20 5 Y
-python3 cr_v2.py 1250 rand_20 5 Y
-python3 cr_v2.py 1500 rand_20 5 Y
-python3 cr_v2.py 500 rand_20 5 N
-python3 cr_v2.py 1000 rand_20 5 N
-python3 cr_v2.py 1250 rand_20 5 N
-python3 cr_v2.py 1500 rand_20 5 N
-
--- exp2/vary_ct.sh
+++
@@ -1,8 +0,0 @@
-python3 cr_v1.py 1500 3
-python3 calc_avg_tt.py
-python3 cr_v1.py 1500 5
-python3 calc_avg_tt.py
-python3 cr_v1.py 1500 10
-python3 calc_avg_tt.py
-python3 cr_v1.py 1500 20
-python3 calc_avg_tt.py
