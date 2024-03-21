0ecbdaa76a980e52cd041483bd701866a001d17e
cmurph77 <91293683+cmurph77@users.noreply.github.com>
2024-03-21 12:40:21 +0000

    added traffic ligths to tree_23_tl

---
+++ reformat/run_tl_sims.sh
@@ -0,0 +1,20 @@
+python3 og_cr.py 500  tree_23_tl 3 2
+python3 og_cr.py 1000 tree_23_tl 3 2
+# python3 og_cr.py 1500 tree_23_tl 3 2
+# python3 og_cr.py 2000 tree_23_tl 3 2
+# python3 og_cr.py 2500 tree_23_tl 3 2
+# python3 og_cr.py 3000 tree_23_tl 3 2
+
+python3 static_astar.py 500  tree_23_tl 3 2
+python3 static_astar.py 1000 tree_23_tl 3 2
+# python3 static_astar.py 1500 tree_23_tl 3 2
+# python3 static_astar.py 2000  tree_23_tl 3 2
+# python3 static_astar.py 2500 tree_23_tl 3 2
+# python3 static_astar.py 3000 tree_23_tl 3 2
+
+python3 so_v1.py 500  tree_23_tl 3 2
+python3 so_v1.py 1000 tree_23_tl 3 2
+# python3 so_v1.py 1500 tree_23_tl 3 2
+# python3 so_v1.py 2000  tree_23_tl 3 2
+# python3 so_v1.py 2500 tree_23_tl 3 2
+# python3 so_v1.py 3000 tree_23_tl 3 2
