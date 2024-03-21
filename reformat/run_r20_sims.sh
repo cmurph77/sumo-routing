46826fcccd7960d3ad917c46fcc2f4b6335e44a4
cmurph77 <91293683+cmurph77@users.noreply.github.com>
2024-03-21 10:25:47 +0000

    added in reformat

---
+++ reformat/run_r20_sims.sh
@@ -0,0 +1,17 @@
+python3 so_v1.py 500 rand_20 5 Y  20.00
+python3 so_v1.py 1000 rand_20 5 Y 20.00
+python3 so_v1.py 1250 rand_20 5 Y 20.00
+python3 so_v1.py 1500 rand_20 5 Y 20.00
+python3 so_v1.py 500 rand_20 5 N  20.00
+python3 so_v1.py 1000 rand_20 5 N 20.00
+python3 so_v1.py 1250 rand_20 5 N 20.00
+python3 so_v1.py 1500 rand_20 5 N 20.00
+
+python3 og_cr.py 500 rand_20 5 Y  15.00
+python3 og_cr.py 1000 rand_20 5 Y 15.00
+python3 og_cr.py 1250 rand_20 5 Y 15.00
+python3 og_cr.py 1500 rand_20 5 Y 15.00
+python3 og_cr.py 500 rand_20 5 N  15.00
+python3 og_cr.py 1000 rand_20 5 N 15.00
+python3 og_cr.py 1250 rand_20 5 N 15.00
+python3 og_cr.py 1500 rand_20 5 N 15.00
---
+++ reformat/run_tr23_sims.sh
@@ -0,0 +1,20 @@
+python3 og_cr.py 500  tree_23 3 N 1.5
+python3 og_cr.py 1000 tree_23 3 N 1.5
+python3 og_cr.py 1500 tree_23 3 N 1.5
+python3 og_cr.py 2000 tree_23 3 N 1.5
+python3 og_cr.py 2500 tree_23 3 N 1.5
+python3 og_cr.py 3000 tree_23 3 N 1.5
+
+python3 og_cr.py 500  tree_23 3 Y 1.5
+python3 og_cr.py 1000 tree_23 3 Y 1.5
+python3 og_cr.py 1500 tree_23 3 Y 1.5
+python3 og_cr.py 2000 tree_23 3 Y 1.5
+python3 og_cr.py 2500 tree_23 3 Y 1.5
+python3 og_cr.py 3000 tree_23 3 Y 1.5
+
+# python3 so_v1.py 500  tree_23 3 Y 
+# python3 so_v1.py 1000 tree_23 3 Y
+# python3 so_v1.py 1500 tree_23 3 Y
+# python3 so_v1.py 2000 tree_23 3 Y
+# python3 so_v1.py 2500 tree_23 3 Y
+# python3 so_v1.py 3000 tree_23 3 Y
