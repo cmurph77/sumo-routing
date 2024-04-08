ed3ffe83b567e0973381a22c27f526c47307bf14
cmurph77 <91293683+cmurph77@users.noreply.github.com>
2024-04-08 18:21:34 +0100

    generated 50 trip files for grid 10 with period 0.1

---
+++ reformat/sim_files/trip_files/trip_files_grid_10/gen_p01_trips.sh
@@ -0,0 +1,100 @@
+# python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+# mv trips.trips.xml 100tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 101tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 102tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 103tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 104tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 105tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 106tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 107tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 108tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 109tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 110tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 111tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 112tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 113tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 114tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 115tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 116tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 117tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 118tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 119tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 120tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 121tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 122tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 123tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 124tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 125tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 126tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 127tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 128tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 129tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 130tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 131tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 132tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 133tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 134tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 135tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 136tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 137tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 138tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 139tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 140tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 141tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 142tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 143tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 144tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 145tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 146tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 147tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 148tr_grid_10.trips.xml
+python3 /opt/homebrew/share/sumo/tools/randomTrips.py -n ../../grid_10.net.xml -e 100 --random --period 0.1
+mv trips.trips.xml 149tr_grid_10.trips.xml
